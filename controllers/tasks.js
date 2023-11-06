6cee29081c552d6210609b7d4c8cb4d3688468cd
Sumit <marvelsumit2002@gmail.com>
2023-11-06 20:28:04 +0530

    second commit

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,36 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Task = require('../models/tasks')
 
 
-const getAllItems = (req, res)=> {
-    res.send('Get all items')
+const getAllItems = async (req, res)=> {
+    try {
+        const task = await Task.find({})
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
 }
-const createTask = (req, res)=> {
-    res.json(req.body)
+const createTask = async (req, res)=> {
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })    
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
 }
-const getTask = (req, res)=> {
-    res.send('Get single item')
-}
+const getTask = async (req, res)=> {
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOne({ _id: taskID })
+        if(!task) {
+            return res.status(404).json({ msg: `No task with id: ${task}` })
+        }
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+} 
 const updateTask = (req, res)=> {
     res.send('Update tasks')
 }
